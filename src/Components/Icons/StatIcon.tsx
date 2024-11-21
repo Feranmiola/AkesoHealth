443c8d91e865fad6dd26635773fe0bc4c677009e
Feranmiola <88761161+Feranmiola@users.noreply.github.com>
2024-11-21 01:06:49 +0100

    "Updated Services component: added StatIcon and PersonIcon, replaced ThrophyIcon instances, and modified text and styles."

---
+++ src/Components/Icons/StatIcon.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const StatIcon = () => {
+    return (
+        <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M12 14.6666V21.3333M18.6666 6.66659V21.3333M25.3333 17.3333V21.3333M5.33331 5.33325V23.9999C5.33331 25.4727 6.52722 26.6666 7.99998 26.6666H28" stroke="#1B7DFA" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+        </svg>
+
+    )
+}
+
+export default StatIcon
--- src/Components/Services.tsx
+++ src/Components/Services.tsx
@@ -2,10 +2,12 @@
 import Image from 'next/image'
 import React from 'react'
 import ThrophyIcon from './Icons/ThrophyIcon'
+import StatIcon from './Icons/StatIcon'
+import PersonIcon from './Icons/PersonIcon'
 
 const Services = () => {
     return (
-        <div className='bg-white h-[938px] w-screen flex flex-col space-y-5 items-center justify-center'>
+        <div className='bg-white h-[938px] w-screen flex flex-col space-y-7 items-center justify-center'>
             <div className='w-[1213.69px] flex flex-row items-end justify-between'>
                 <p className='text-[45.59px] text-light-blue font-medium leading-[2.5rem] font-plus-jakarta'>Akeso, because healthcare<br /> deserves better.</p>
                 <p className='text-gray2 text-base text-center'>At Akeso, we don’t just fix what’s broken—we build <br />smarter solutions that put care back in healthcare.</p>
@@ -21,20 +23,20 @@ const Services = () => {
             <div className='w-[1200px] flex items-center justify-between h-[120px]'>
                 <div className='h-full w-[291px] flex flex-col justify-between'>
                     <ThrophyIcon />
-                    <p className='text-gray font-mono text-base'>Operational Excellence</p>
+                    <p className='text-gray font-mono text-base font-medium'>Operational Excellence</p>
                     <p className='text-sm text-gray2 font-inter-var tracking-tight w-full'>We simplify healthcare operations by streamlining workflows, improving provider efficiency, and reducing administrative costs.</p>
                 </div>
 
                 <div className='h-full w-[291px] flex flex-col justify-between'>
-                    <ThrophyIcon />
-                    <p className='text-gray font-mono text-base'>Operational Excellence</p>
-                    <p className='text-sm text-gray2 font-inter-var tracking-tight w-full'>We simplify healthcare operations by streamlining workflows, improving provider efficiency, and reducing administrative costs.</p>
+                    <StatIcon />
+                    <p className='text-gray font-mono text-base font-medium'>Data-Driven Health Insights</p>
+                    <p className='text-sm text-gray2 font-inter-var tracking-tight w-full'>Our actionable analytics and evidence-based insights empower effective policies and better population health management.</p>
                 </div>
 
                 <div className='h-full w-[291px] flex flex-col justify-between'>
-                    <ThrophyIcon />
-                    <p className='text-gray font-mono text-base'>Operational Excellence</p>
-                    <p className='text-sm text-gray2 font-inter-var tracking-tight w-full'>We simplify healthcare operations by streamlining workflows, improving provider efficiency, and reducing administrative costs.</p>
+                    <PersonIcon />
+                    <p className='text-gray font-mono text-base font-medium'>Improved Patient Outcomes</p>
+                    <p className='text-sm text-gray2 font-inter-var tracking-tight w-full'>With coordinated care plans and integrated health records, we enhance safety, improve care quality, and boost patient satisfaction.</p>
                 </div>
 
             </div>
