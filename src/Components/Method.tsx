173b04d32272e84021d07a43d11764c3096da7e9
Feranmiola <88761161+Feranmiola@users.noreply.github.com>
2024-11-21 09:48:03 +0100

    "Updated Method component: changed text colors, content, and image sources for three sections."

--- src/Components/Method.tsx
+++ src/Components/Method.tsx
@@ -54,7 +54,7 @@ const Method = () => {
 
             <motion.div className='h-full flex flex-col justify-between w-[539px]' variants={containerVariants}>
                 <motion.div className='w-full h-[409px] flex flex-col justify-between' variants={itemVariants}>
-                    <motion.p className='text-base font-mono text-[#9DC1FF]' variants={itemVariants}>
+                    <motion.p className='text-base font-mono text-[#DEEAFF]' variants={itemVariants}>
                         [Simpler Healthcare for Providers]
                     </motion.p>
                     <motion.p className='text-pale text-base' variants={itemVariants}>
@@ -77,71 +77,71 @@ const Method = () => {
                 </motion.div>
 
                 <motion.div className='w-full h-[409px] flex flex-col justify-between' variants={itemVariants}>
-                    <motion.p className='text-base font-mono text-[#9DC1FF]' variants={itemVariants}>
-                        [Simpler Healthcare for Providers]
+                    <motion.p className='text-base font-mono text-[#DEEAFF]' variants={itemVariants}>
+                        [Seamless Communication Across Systems]
                     </motion.p>
                     <motion.p className='text-pale text-base' variants={itemVariants}>
-                        We reduce paperwork and streamline tasks so <br /> doctors can spend more time with you.
+                        Your health data flows securely between healthcare providers <br /> so you never have to repeat tests or retell your story.
                     </motion.p>
                     <motion.div
 
                     >
                         <Image
-                            src='https://res.cloudinary.com/debiu7z1b/image/upload/v1732120974/Frame_12_wr9jrr.webp'
-                            alt='Doctor'
+                            src='https://res.cloudinary.com/debiu7z1b/image/upload/v1732178570/confident-mature-radiologist-mask-pointing-brain-scan-tablet-screen_1_avpd9z.webp'
+                            alt='Doctor 2'
                             width={539}
                             height={287}
                             className='rounded-[16px]'
                         />
                     </motion.div>
                     <motion.p className='font-bold text-base text-pale' variants={itemVariants}>
-                        Imagine your doctor actually having time to listen —<br />Akeso makes that possible.
+                        Your information, always where it’s needed,<br />when it’s needed.
                     </motion.p>
                 </motion.div>
 
                 <motion.div className='w-full h-[409px] flex flex-col justify-between' variants={itemVariants}>
-                    <motion.p className='text-base font-mono text-[#9DC1FF]' variants={itemVariants}>
-                        [Simpler Healthcare for Providers]
+                    <motion.p className='text-base font-mono text-[#DEEAFF]' variants={itemVariants}>
+                        [Long-Term Care That’s Proactive]
                     </motion.p>
                     <motion.p className='text-pale text-base' variants={itemVariants}>
-                        We reduce paperwork and streamline tasks so <br /> doctors can spend more time with you.
+                        With personalized care plans and continuous monitoring, we<br /> help you stay on track and prevent issues before they arise.
                     </motion.p>
                     <motion.div
 
                     >
                         <Image
-                            src='https://res.cloudinary.com/debiu7z1b/image/upload/v1732120974/Frame_12_wr9jrr.webp'
-                            alt='Doctor'
+                            src='https://res.cloudinary.com/debiu7z1b/image/upload/v1732178570/social-worker-taking-care-senior-woman_1_b73qju.webp'
+                            alt='Doctor 3'
                             width={539}
                             height={287}
                             className='rounded-[16px]'
                         />
                     </motion.div>
                     <motion.p className='font-bold text-base text-pale' variants={itemVariants}>
-                        Imagine your doctor actually having time to listen —<br />Akeso makes that possible.
+                        Your health is more than a single visit—it’s —<br />a lifelong journey.
                     </motion.p>
                 </motion.div>
 
                 <motion.div className='w-full h-[409px] flex flex-col justify-between' variants={itemVariants}>
-                    <motion.p className='text-base font-mono text-[#9DC1FF]' variants={itemVariants}>
-                        [Simpler Healthcare for Providers]
+                    <motion.p className='text-base font-mono text-[#DEEAFF]' variants={itemVariants}>
+                        [Better Community Health Insights]
                     </motion.p>
                     <motion.p className='text-pale text-base' variants={itemVariants}>
-                        We reduce paperwork and streamline tasks so <br /> doctors can spend more time with you.
+                        We work with public health experts to analyze <br /> trends and improve health at a larger scale.
                     </motion.p>
                     <motion.div
 
                     >
                         <Image
-                            src='https://res.cloudinary.com/debiu7z1b/image/upload/v1732120974/Frame_12_wr9jrr.webp'
-                            alt='Doctor'
+                            src='https://res.cloudinary.com/debiu7z1b/image/upload/v1732178571/doctors-medical-professionals-healthcare-workers-with-laptop-talking-meeting-planning-hospital-medicine-treatment-view-diverse-group-physician-colleagues-brainstorming-with-tech_i6llhu.webp'
+                            alt='Doctor 4'
                             width={539}
                             height={287}
                             className='rounded-[16px]'
                         />
                     </motion.div>
                     <motion.p className='font-bold text-base text-pale' variants={itemVariants}>
-                        Imagine your doctor actually having time to listen —<br />Akeso makes that possible.
+                        From individual care to community well-being,<br />Akeso makes a difference.
                     </motion.p>
                 </motion.div>
             </motion.div>
